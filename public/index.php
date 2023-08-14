ff1c0e007ff46154d0d16cce3555ee50612d3260
Benoit Foujols <benoit@foujols.com>
2023-08-14 20:37:27 +0200

    #12 add new class ConfigCore

--- public/index.php
+++ public/index.php
@@ -7,28 +7,36 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 // Utilisation des classes utilisées dans le fichier
 use Dotenv\Dotenv;
+use Studoo\EduFramework\Core\ConfigCore;
 use Studoo\EduFramework\Core\Controller\FastRouteCore;
 use Studoo\EduFramework\Core\Service\DatabaseService;
 use Studoo\EduFramework\Core\View\TwigCore;
 
+(new ConfigCore([
+    'base_path'         => __DIR__ . '/../',
+    'twig_path'         => __DIR__ . '/../app/Template',
+    "route_config_path"  => __DIR__ . '/../app/config/',
+]));
+
+
 // Gestion des fichiers environnement .env
-$dotenv = Dotenv::createImmutable(__DIR__ . "/../");
+$dotenv = Dotenv::createImmutable(ConfigCore::getConfig('base_path'));
 $dotenv->load();
 
 // Gestion de la couche View
-(new TwigCore(__DIR__ . '/../app/Template'));
+(new TwigCore(ConfigCore::getConfig('twig_path')));
 $en = TwigCore::getEnvironment();
 
 
 // Gestion de la couche Model et de la connexion à la base de données
-if ($_ENV['DB_HOST_STATUS'] === 'true') {
+if (ConfigCore::getEnv('DB_HOST_STATUS') === 'true') {
     (new DatabaseService());
 }
 
 // Gestion des routes
 $route = new FastRouteCore();
-// Load des routes depuis le fichier de configuration
-$route->loadRouteConfig(__DIR__ . '/../app/config/');
+// LoadCore des routes depuis le fichier de configuration
+$route->loadRouteConfig(ConfigCore::getConfig('route_config_path'));
 
 // Récupération de la route à appeler
 try {
