240b9927444cc69cacd4b541fe6ff98235c82c76
JPechberty <Ninposhine313>
2023-08-18 01:07:07 +0200

    #25 Refact DockerPilot for Qodana compliance

--- src/EduFramework/Scripts/DockerPilot.php
+++ src/EduFramework/Scripts/DockerPilot.php
@@ -13,44 +13,47 @@ class DockerPilot
     private const DOCKER_START_INSTRUCTION = "up -d";
     private const DOCKER_DOWN_INSTRUCTION = "down";
 
-
+    /**
+     * @param Event $event
+     * @param string $instruction
+     * @return string|null
+     * @throws DockerPilotInvalidArgumentException
+     */
     private static function buildCommand(Event $event, string $instruction): ?string
     {
-        $dockerComposeFileName = match($event->getArguments()[0] ?? null) {
+        $dockerComposeFileName = match ($event->getArguments()[0] ?? null) {
             'mysql' => self::DOCKER_COMPOSE_MYSQL_RECIPE,
             'maria-db' => self::DOCKER_COMPOSE_MARIADB_RECIPE,
             default => null
         };
 
-        return isset($dockerComposeFileName) ?
-            "docker compose -f $dockerComposeFileName  --env-file .env $instruction" :
-            null;
+        if(!isset($dockerComposeFileName)){
+            throw new DockerPilotInvalidArgumentException();
+        }
+
+        return "docker compose -f $dockerComposeFileName  --env-file .env $instruction";
     }
 
     /**
+     * @param Event $event
+     * @return void
      * @throws DockerPilotInvalidArgumentException
      */
     public static function start(Event $event): void
     {
         $command = self::buildCommand($event,self::DOCKER_START_INSTRUCTION);
-
-        if(!isset($command)){
-            throw new DockerPilotInvalidArgumentException();
-        }
         exec($command);
 
     }
 
     /**
+     * @param Event $event
+     * @return void
      * @throws DockerPilotInvalidArgumentException
      */
     public static function down(Event $event): void
     {
         $command = self::buildCommand($event,self::DOCKER_DOWN_INSTRUCTION);
-
-        if(!isset($command)){
-            throw new DockerPilotInvalidArgumentException();
-        }
         exec($command);
     }
 
