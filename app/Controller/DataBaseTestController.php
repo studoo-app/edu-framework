69a689bbb2ba04dd836c032b62ed49d24a7477fc
Benoit Foujols <benoit@foujols.com>
2023-12-28 19:20:31 +0100

    #24 Exemple code db

---
+++ app/Controller/DataBaseTestController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Controller;
+
+use Studoo\EduFramework\Core\Controller\ControllerInterface;
+use Studoo\EduFramework\Core\Controller\Request;
+use Studoo\EduFramework\Core\Service\DatabaseService;
+use Studoo\EduFramework\Core\View\TwigCore;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+
+class DataBaseTestController implements ControllerInterface
+{
+    /**
+     * @param Request $request Requête HTTP
+     * @return string|null
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     * @throws \Exception
+     */
+    public function execute(Request $request): string|null
+    {
+        // Connexion à la base de données
+        $getConnectDb = DatabaseService::getConnect();
+        // Requête SQL
+        $etudiantStmt = $getConnectDb->prepare("SELECT * FROM `etudiant`");
+        // Exécution de la requête
+        $etudiantStmt->execute();
+        // Récupération des données
+        $etudiants = $etudiantStmt->fetchAll();
+
+        return TwigCore::getEnvironment()->render('home/db.html.twig',
+            [
+                'titre'   => 'Exemple avec connextion à la base de données',
+                'etudiants' => $etudiants
+            ]
+        );
+    }
+}
