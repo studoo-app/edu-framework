348860d22640f13fc0f830bab99c2018f36a2e3e
Benoit Foujols <benoit@foujols.com>
2024-06-05 22:36:40 +0200

    #103 add testunit sqlite

--- tests/Service/DatabaseServiceSqliteTest.php
+++ tests/Service/DatabaseServiceSqliteTest.php
@@ -8,21 +8,33 @@ use Studoo\EduFramework\Core\ConfigCore;
 use Studoo\EduFramework\Core\Exception\ErrorDatabaseNotExistException;
 use Studoo\EduFramework\Core\Service\DatabaseService;
 use PHPUnit\Framework\TestCase;
+use Studoo\EduFramework\Core\Service\DatabaseSqlite;
 
 class DatabaseServiceSqliteTest extends TestCase
 {
 
-    public function testInstanceOfPdo()
+    public function setUp(): void
     {
         $dotenv = Dotenv::createImmutable(__DIR__ . '/../Config/');
         $dotenv->load();
         (new ConfigCore([
             'sqlite_path' => './var/sqlite/'
         ]));
-
         $_ENV["DB_TYPE"] = "sqlite";
+    }
 
+    public function testInstanceOfPdo()
+    {
         new DatabaseService();
         $this->assertInstanceOf(PDO::class, DatabaseService::getConnect());
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function testGetNewConnectSqlite()
+    {
+       $connection = new DatabaseSqlite();
+        $this->assertInstanceOf(PDO::class, $connection->getManager());
     }
 }
