77bae454d7106bb18fd3bdec41c82184d9bdf76e
Benoit Foujols <benoit@foujols.com>
2024-06-15 11:53:09 +0200

    #87 add test GetNameToPath

--- tests/Controller/FastRouteCoreTest.php
+++ tests/Controller/FastRouteCoreTest.php
@@ -89,4 +89,15 @@ class FastRouteCoreTest extends TestCase
         $_SERVER['REQUEST_URI'] = '/';
         $this->assertEquals('ed2ad24f02864b53319ab611d414ceac5c322751', sha1($route->getRoute()));
     }
+
+    public function testLinkToGetnametopathInTwig()
+    {
+        $route = new FastRouteCore();
+        $route->loadRouteConfig(__DIR__ . '/../Config/');
+
+        $_SERVER['REQUEST_METHOD'] = 'GET';
+        $_SERVER['REQUEST_URI'] = '/link';
+
+        $this->assertEquals('1ab50275de77fa6215e882b0833350c32cfcd7a2', sha1($route->getRoute()));
+    }
 }
